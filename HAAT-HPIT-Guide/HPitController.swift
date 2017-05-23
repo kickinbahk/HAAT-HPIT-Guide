cfebcd835ddee1599355697aa6644bf6a22ca3cb
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-05-23 07:06:20 -0700

    rename home controller, add button to pit tableview

---
+++ HAAT-HPIT-Guide/HPitController.swift
@@ -0,0 +1,20 @@
+//
+//  HPitController.swift
+//  HAAT-HPIT-Guide
+//
+//  Created by Josiah Mory on 5/23/17.
+//  Copyright © 2017 kickinbahk Productions. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HPitController: UITableViewController {
+
+  @IBOutlet weak var closeButton: UIBarButtonItem!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+}
