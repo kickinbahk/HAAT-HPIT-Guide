f66c032ea4fab4d9095bc258acf8d2bdc8491ee9
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-07-22 09:30:47 -0700

    add button to storyboard for tankcontroller

---
+++ HAAT-HPIT-Guide/HTankController.swift
@@ -0,0 +1,19 @@
+//
+//  HTankController.swift
+//  HAAT-HPIT-Guide
+//
+//  Created by Josiah Mory on 7/22/17.
+//  Copyright © 2017 kickinbahk Productions. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HTankController: UITableViewController {
+ 
+  @IBOutlet weak var closeButton: UIBarButtonItem!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
